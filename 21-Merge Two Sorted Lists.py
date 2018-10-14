ee3be43bd1cf580eb8d289291fbe8e18bd027b3d
phenix <zyf.2960511894@outlook.com>
2018-10-14 15:37:12 +0800

    Create 21-Merge Two Sorted Lists.py

---
+++ 21-Merge Two Sorted Lists.py
@@ -0,0 +1,37 @@
+'''
+将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+示例：
+输入：1->2->4, 1->3->4
+输出：1->1->2->3->4->4
+'''
+
+# by others
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        if l1 == None:
+            return l2
+        if l2 == None:
+            return l1
+        
+        dummy = ListNode(-1)
+        cur = dummy
+        
+        while l1 and l2:
+            if l1.val < l2.val:
+                cur.next = l1
+                l1 = l1.next
+            else:
+                cur.next = l2
+                l2 = l2.next
+            cur = cur.next
+        
+        if l1:
+            cur.next = l1
+        else:
+            cur.next = l2
+        return dummy.next
