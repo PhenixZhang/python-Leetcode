e0c50858b80591c08d4f3b7bd3e83f56f33426af
phenix <zyf.2960511894@outlook.com>
2018-10-17 12:45:12 +0800

    Create 53-Maximum Subarray.py

---
+++ 53-Maximum Subarray.py
@@ -0,0 +1,20 @@
+'''
+给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+示例:
+输入: [-2,1,-3,4,-1,2,1,-5,4],
+输出: 6
+解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+'''
+
+# by others
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        maxSum = [nums[0] for i in range(n)]
+        for i in range(1, n):
+            maxSum[i] = max(maxSum[i - 1] + nums[i], nums[i])
+        return max(maxSum)
