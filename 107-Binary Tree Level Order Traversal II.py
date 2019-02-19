f0afcea67b41d01f40cfabb16cfb957b0c532591
phenix <zyf.2960511894@outlook.com>
2019-02-19 09:36:35 +0800

    Create 107-Binary Tree Level Order Traversal II.py

---
+++ 107-Binary Tree Level Order Traversal II.py
@@ -0,0 +1,44 @@
+'''
+给定一个二叉树，返回其节点值自底向上的层次遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
+
+例如：
+给定二叉树 [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回其自底向上的层次遍历为：
+
+[
+  [15,7],
+  [9,20],
+  [3]
+]
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def levelOrderBottom(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        def dfs(node,level,res):
+            if not node:
+                return
+            if len(res) < level:
+                res.append([])
+            res[level - 1].append(node.val)
+            dfs(node.left,level + 1,res)
+            dfs(node.right,level + 1,res)
+        
+        res = []
+        dfs(root,1,res)
+        return res[::-1]
