c9afecc5063750597fd37a6e3cf8501ed4de1cf8
phenix <zyf.2960511894@outlook.com>
2019-02-19 09:50:19 +0800

    Create 110-Balanced Binary Tree.py

---
+++ 110-Balanced Binary Tree.py
@@ -0,0 +1,38 @@
+'''
+给定一个二叉树，判断它是否是高度平衡的二叉树。
+
+本题中，一棵高度平衡二叉树定义为：
+
+一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过1。
+
+示例 1:
+
+给定二叉树 [3,9,20,null,null,15,7]
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回 true 。
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isBalanced(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        def height(node):
+            if not node:
+                return 0
+            return 1 + max(height(node.left),height(node.right))
+        if not root:
+            return True
+        return abs(height(root.left) - height(root.right)) <= 1 and self.isBalanced(root.left) and self.isBalanced(root.right)
