d8e9ac32e12df31d93056eb0095716f514217e31
phenix <zhangphenix123@gmail.com>
2019-03-14 13:24:48 +0800

    Update 28-Implement strStr().py

--- 28-Implement strStr().py
+++ 28-Implement strStr().py
@@ -27,6 +27,26 @@ class Solution:
         else:
             return -1
 
+# by myself
+class Solution:
+    def strStr(self, haystack, needle):
+        len1 = len(haystack)
+        len2 = len(needle)
+        if len2 == 0:
+            return 0
+        if len1 == 0 or len1 < len2:
+            return -1
+        if len1 == len2:
+            if haystack == needle:
+                return 0
+            else:
+                return -1
+        for i in range(len1 - len2 + 1):
+            if haystack[i:i+len2] == needle:
+                return i
+        else:
+            return -1
+        
 # by others
 class Solution(object):
     def strStr(self, haystack, needle):
